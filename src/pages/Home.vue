1a56b5d075aa9ad956bbdfd5d438ab862a554881
unknown <candradwiprasetyo@gmail.com>
2025-04-24 10:53:35 +0700

    style: adjust responsiveness

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-6 mx-auto w-[600px] min-h-[300px]">
+  <div class="p-4 md:p-6 mx-auto w-full md:w-[600px] min-h-[300px]">
     <Tab v-model:activeTab="active" />
     <WeightConverter v-show="active === 'weight'" />
     <TemperatureConverter v-show="active === 'temperature'" />
